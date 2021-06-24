6e057b8f8044faebc24b32e5400a94373f604470
Trauvin <mateusgalote@gmail.com>
2021-06-24 18:36:29 -0300

    books upload system

--- django-upload-files/mysite/core/views.py
+++ django-upload-files/mysite/core/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, ListView, CreateView
 from django.core.files.storage import FileSystemStorage
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import BookForm
+from .models import Book
+
+
 class Home(TemplateView):
     template_name = 'home.html'
 
@@ -15,3 +19,22 @@ def upload(request):
         context['url'] = fs.url(name)
         
     return render(request, 'upload.html', context)
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {
+        'books': books
+    })
+
+
+def upload_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else :
+        form = BookForm()
+    return render(request, 'upload_book.html', {
+        'form': form,
+    })
--- django-upload-files/mysite/settings.py
+++ django-upload-files/mysite/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'mysite.core',
 ]
+
+CRYSPY_TEMPLATE_PACK = 'boostrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
