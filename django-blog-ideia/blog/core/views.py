cca81fc83bb489e10f22846c494ed8d69bb3845c
Trauvin <mateusgalote@gmail.com>
2021-06-26 16:40:35 -0300

    adding blog views

--- django-blog-ideia/blog/core/views.py
+++ django-blog-ideia/blog/core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Post, Comment
 
-# Create your views here.
+def blog_index(request):
+    post = Post.objects.all().order_by('-created_on')
+    context = {
+        'post': post
+    }
+    return render(request, 'blog_index.html', context)
+
+def blog_category(request, category):
+    posts = Post.objects.filter(categories__name__contains=category).order_by('-created_on')
+    context = {
+        "category": category,
+        "posts": posts
+    }
+    return render(request, "blog_category.html", context)
