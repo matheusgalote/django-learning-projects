b12fab117cc0cadfddaa36b3ae7d8b0c2466ddd7
Trauvin <mateusgalote@gmail.com>
2021-06-30 22:01:47 -0300

    ad

---
+++ django-user-management/awesome_website/users/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import include, url
+from users.views import dashboard
+
+urlpatterns = [
+    url(r"^accounts/", include("django.contrib.auth.urls")),
+    url(r"^dashboard/", dashboard, name="dashboard"),
+]
