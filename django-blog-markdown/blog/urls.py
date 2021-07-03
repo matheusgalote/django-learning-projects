77b38a621e0c7519edc3c3b22c6dd9befb708660
Trauvin <mateusgalote@gmail.com>
2021-07-03 09:24:27 -0300

    add detail page

--- django-blog-markdown/blog/urls.py
+++ django-blog-markdown/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.blog_index, name='blog_index')
+    path('', views.blog_index, name='blog_index'),
+    path('<int:pk>', views.blog_detail, name='blog_detail')
 ]
--- django-blog-markdown/blog/views.py
+++ django-blog-markdown/blog/views.py
@@ -4,3 +4,7 @@ from .models import Post
 def blog_index(request):
     posts = Post.objects.all()
     return render(request, 'blog_index.html', {'posts': posts})
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog_detail.html', {'post': post})
