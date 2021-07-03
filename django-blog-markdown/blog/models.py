82cd57e619246673af885bc1211bbf4eba145e7a
Trauvin <mateusgalote@gmail.com>
2021-07-03 12:00:30 -0300

    layout adjusts

--- django-blog-markdown/blog/models.py
+++ django-blog-markdown/blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField(null=True, blank=True)
+    category_img = models.ImageField(upload_to='imgs/categories/', null=True, blank=True)
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -11,4 +12,4 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
-    cover_img = models.ImageField(upload_to='imgs/covers/')
+    cover_img = models.ImageField(upload_to='imgs/covers/', null=True, blank=True)
